817763a17c0cb05bd48f251d5c7e6535f329deff
huangyingfeng25-maker <huangyingfeng25@gmail.com>
2026-08-26 18:22:44 +0800

    feat: 初始化项目

---
+++ lease/web/web-admin/src/main/java/com/atguigu/lease/AdminWebApplication.java
@@ -0,0 +1,12 @@
+package com.atguigu.lease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class AdminWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminWebApplication.class, args);
+    }
+}
---
+++ lease/web/web-app/src/main/java/com/atguigu/lease/AppWebApplication.java
@@ -0,0 +1,12 @@
+package com.atguigu.lease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class AppWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppWebApplication.class, args);
+    }
+}
