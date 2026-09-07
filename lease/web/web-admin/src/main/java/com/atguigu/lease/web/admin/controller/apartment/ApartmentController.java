f0b15419d2ba6ab48a359dbd29b52461d282a608
huangyingfeng25-maker <huangyingfeng25@gmail.com>
2026-09-07 16:34:20 +0800

    更新公寓信息接口完成

--- lease/web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/ApartmentController.java
+++ lease/web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/ApartmentController.java
@@ -9,6 +9,7 @@ import com.atguigu.lease.web.admin.vo.apartment.ApartmentDetailVo;
 import com.atguigu.lease.web.admin.vo.apartment.ApartmentItemVo;
 import com.atguigu.lease.web.admin.vo.apartment.ApartmentQueryVo;
 import com.atguigu.lease.web.admin.vo.apartment.ApartmentSubmitVo;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.v3.oas.annotations.Operation;
@@ -61,6 +62,10 @@ public class ApartmentController {
     @Operation(summary = "根据id修改公寓发布状态")
     @PostMapping("updateReleaseStatusById")
     public Result updateReleaseStatusById(@RequestParam Long id, @RequestParam ReleaseStatus status) {
+        LambdaUpdateWrapper<ApartmentInfo>wrapper=new LambdaUpdateWrapper<>();
+        wrapper.eq(ApartmentInfo::getId,id);
+        wrapper.set(ApartmentInfo::getIsRelease,status);
+        apartmentInfoService.update(wrapper);
         return Result.ok();
     }
 
