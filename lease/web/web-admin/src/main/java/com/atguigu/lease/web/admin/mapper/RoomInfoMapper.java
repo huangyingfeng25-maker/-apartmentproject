c0f4b2deccb29f2c2c4aa5ae7d4649334514a12b
huangyingfeng25-maker <huangyingfeng25@gmail.com>
2026-09-10 13:18:56 +0800

    Revert "根据id查询房间详情接口完成"
    This reverts commit ce16233ebda444d047409920f3b70ba4bd24d644.

--- lease/web/web-admin/src/main/java/com/atguigu/lease/web/admin/mapper/RoomInfoMapper.java
+++ lease/web/web-admin/src/main/java/com/atguigu/lease/web/admin/mapper/RoomInfoMapper.java
@@ -5,7 +5,6 @@ import com.atguigu.lease.web.admin.vo.room.RoomItemVo;
 import com.atguigu.lease.web.admin.vo.room.RoomQueryVo;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 
 /**
 * @author liubo
@@ -15,8 +14,6 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 */
 public interface RoomInfoMapper extends BaseMapper<RoomInfo> {
 
-    //根据条件分页查询房间列表
-    IPage<RoomItemVo> selectRoomInfoPage(Page<RoomItemVo> page, RoomQueryVo queryVo);
 }
 
 
