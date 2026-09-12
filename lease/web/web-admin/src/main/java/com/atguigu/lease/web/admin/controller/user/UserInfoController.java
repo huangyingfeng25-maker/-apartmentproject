07c404517bf0409ba42daa29f2455fce5bcd87ed
huangyingfeng25-maker <huangyingfeng25@gmail.com>
2026-09-12 14:51:30 +0800

    分页查询用户信息接口完成

--- lease/web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/user/UserInfoController.java
+++ lease/web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/user/UserInfoController.java
@@ -4,10 +4,16 @@ package com.atguigu.lease.web.admin.controller.user;
 import com.atguigu.lease.common.result.Result;
 import com.atguigu.lease.model.entity.UserInfo;
 import com.atguigu.lease.model.enums.BaseStatus;
+import com.atguigu.lease.web.admin.service.UserInfoService;
 import com.atguigu.lease.web.admin.vo.user.UserInfoQueryVo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "用户信息管理")
@@ -15,10 +21,25 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/admin/user")
 public class UserInfoController {
 
+    @Autowired
+    private UserInfoService userInfoService;
+
     @Operation(summary = "分页查询用户信息")
     @GetMapping("page")
-    public Result<IPage<UserInfo>> pageUserInfo(@RequestParam long current, @RequestParam long size, UserInfoQueryVo queryVo) {
-        return Result.ok();
+    public Result<IPage<UserInfo>> pageUserInfo(@RequestParam long current,
+                                                @RequestParam long size,
+                                                UserInfoQueryVo queryVo) {
+        Page<UserInfo> pageParam = new Page<>(current,size);
+        LambdaQueryWrapper<UserInfo> wrapper = new LambdaQueryWrapper<>();
+
+        if(StringUtils.hasText(queryVo.getPhone())) {
+            wrapper.eq(UserInfo::getPhone,queryVo.getPhone());
+        }
+        if(queryVo.getStatus() != null){
+            wrapper.eq(UserInfo::getStatus,queryVo.getStatus());
+        }
+        IPage<UserInfo> pageModel = userInfoService.page(pageParam,wrapper);
+        return Result.ok(pageModel);
     }
 
     @Operation(summary = "根据用户id更新账号状态")
