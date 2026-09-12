2c84fae779e321e769394e78fda7cd3d808f9e20
huangyingfeng25-maker <huangyingfeng25@gmail.com>
2026-09-12 15:14:05 +0800

    根据ID查询后台用户信息接口完成

--- lease/web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/SystemUserService.java
+++ lease/web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/SystemUserService.java
@@ -16,4 +16,6 @@ public interface SystemUserService extends IService<SystemUser> {
 
 
     IPage<SystemUserItemVo> selectPageSystemUser(Page<SystemUserItemVo> page, SystemUserQueryVo queryVo);
+
+    SystemUserItemVo getSystemUserById(Long id);
 }
--- lease/web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/SystemUserServiceImpl.java
+++ lease/web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/SystemUserServiceImpl.java
@@ -1,13 +1,16 @@
 package com.atguigu.lease.web.admin.service.impl;
 
+import com.atguigu.lease.model.entity.SystemPost;
 import com.atguigu.lease.model.entity.SystemUser;
 import com.atguigu.lease.web.admin.mapper.SystemUserMapper;
+import com.atguigu.lease.web.admin.service.SystemPostService;
 import com.atguigu.lease.web.admin.service.SystemUserService;
 import com.atguigu.lease.web.admin.vo.system.user.SystemUserItemVo;
 import com.atguigu.lease.web.admin.vo.system.user.SystemUserQueryVo;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,10 +25,23 @@ public class SystemUserServiceImpl extends ServiceImpl<SystemUserMapper, SystemU
 
     @Autowired
     private SystemUserMapper systemUserMapper;
+    @Autowired
+    private SystemPostService systemPostService;
 
     @Override
     public IPage<SystemUserItemVo> selectPageSystemUser(Page<SystemUserItemVo> page, SystemUserQueryVo queryVo) {
         return systemUserMapper.selectPageSystemUser(page,queryVo);
+    }
+
+    @Override
+    public SystemUserItemVo getSystemUserById(Long id) {
+        SystemUser systemUser = getById(id);
+        systemUser.setPassword(null);
+        SystemPost systemPost = systemPostService.getById(systemUser.getPostId());
+        SystemUserItemVo systemUserItemVo =  new SystemUserItemVo();
+        BeanUtils.copyProperties(systemUser,systemUserItemVo);
+        systemUserItemVo.setPostName(systemPost.getName());
+        return systemUserItemVo;
     }
 }
 
