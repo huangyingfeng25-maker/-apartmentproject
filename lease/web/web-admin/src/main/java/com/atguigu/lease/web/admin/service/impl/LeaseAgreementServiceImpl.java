ced076e654ac69214f428f68e6810c892aa2e42c
huangyingfeng25-maker <huangyingfeng25@gmail.com>
2026-09-12 11:17:25 +0800

    根据条件分页查询租约列表接口完成

--- lease/web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/LeaseAgreementServiceImpl.java
+++ lease/web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/LeaseAgreementServiceImpl.java
@@ -3,7 +3,12 @@ package com.atguigu.lease.web.admin.service.impl;
 import com.atguigu.lease.model.entity.LeaseAgreement;
 import com.atguigu.lease.web.admin.mapper.LeaseAgreementMapper;
 import com.atguigu.lease.web.admin.service.LeaseAgreementService;
+import com.atguigu.lease.web.admin.vo.agreement.AgreementQueryVo;
+import com.atguigu.lease.web.admin.vo.agreement.AgreementVo;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,6 +20,14 @@ import org.springframework.stereotype.Service;
 public class LeaseAgreementServiceImpl extends ServiceImpl<LeaseAgreementMapper, LeaseAgreement>
         implements LeaseAgreementService {
 
+    @Autowired
+    private LeaseAgreementMapper leaseAgreementMapper;
+
+    //根据条件分页查询租约列表
+    @Override
+    public IPage<AgreementVo> selectPageAgreement(Page<AgreementVo> page, AgreementQueryVo queryVo) {
+        return leaseAgreementMapper.selectPageAgreement(page,queryVo);
+    }
 }
 
 
