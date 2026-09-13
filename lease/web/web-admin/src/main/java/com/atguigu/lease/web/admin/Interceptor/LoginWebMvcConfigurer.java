aba9cdbf5267b1aac6b4651bd6aa1f1e757ee9fd
huangyingfeng25-maker <huangyingfeng25@gmail.com>
2026-09-13 17:09:39 +0800

    拦截器的实现（是否登录判断）

---
+++ lease/web/web-admin/src/main/java/com/atguigu/lease/web/admin/Interceptor/LoginWebMvcConfigurer.java
@@ -0,0 +1,20 @@
+package com.atguigu.lease.web.admin.Interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class LoginWebMvcConfigurer implements WebMvcConfigurer {
+
+    @Autowired
+    private AuthenticationInterceptor authenticationInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor)
+                .addPathPatterns("/admin/**")
+                .excludePathPatterns("/admin/login/**");
+    }
+}
